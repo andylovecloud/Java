200a2e7bff3e056419ca67f1093e2aec0d812259
andylovecloud <andy.tranhuynh@gmail.com>
2023-11-23 09:06:40 +0200

    calling methods from other class

--- Pro1/Pro1/src/Pro_pkg1/ArrayList_advance.java
+++ Pro1/Pro1/src/Pro_pkg1/ArrayList_advance.java
@@ -9,25 +9,53 @@ public class ArrayList_advance {
 		        ArrayList<String> shoppingList = new ArrayList<>();
 		        Scanner scanner = new Scanner(System.in);
 
+		        int i = 1;
+//		        int totalItem = 0;
 		        while (true) 
 		        {
-		            System.out.print("Add item (x = exit): ");
+		            System.out.print("Add item " + i + " (type q to quit): ");
 		            String item = scanner.nextLine();
 
-		            if (item.equals("x")) 
+		            if (item.equals("q")) 
 		            {
 		                break;
 		            }
 		            else 
 		            {
 		                shoppingList.add(item);
+		                i += 1;
+//		                totalItem += 1;
 		            }
 		        }
 
-//		        System.out.println("\nShopping List:");
-		        for (String item : shoppingList) 
-		        {
-		            System.out.println(item);
-		        }
-		    }
+		        System.out.println("\nShopping Cart:");
+		        int totalitems = shoppingList.size();
+				for(int j = 0; j < shoppingList.size();j++)
+				{
+			        for (String item : shoppingList) 
+			        {
+			        	System.out.println("Item " + (j+1) + ": " + item);
+			        	j++;
+			        }
+				}
+				System.out.println("Items total: " + totalitems);
+				
+				System.out.println("Type the number of item you want to delete from the list");
+				int deleteitem = scanner.nextInt();
+				 shoppingList.remove(deleteitem-1);
+				
+//				shoppingList.remove(Integer.valueOf(deleteitem)); 
+				
+				  int newtotalitems = shoppingList.size();
+				 for(int j = 0; j < shoppingList.size();j++)
+					{
+				        for (String item : shoppingList) 
+				        {
+				        	System.out.println("Item " + (j+1) + ": " + item);
+				        	j++;
+				        }
+					}
+					System.out.println("Items total: " + newtotalitems);
+					
 	}
+}
